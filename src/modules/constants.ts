66d620c7329581ce86e609a2dd661037ac9fbb85
lott <lott.marcos@gmail.com>
2026-02-19 08:21:51 +0100

    refactor: simplify WhatsApp link generation by using a constant for the number

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,10 +1,12 @@
 import { getEnv } from "./utils";
 
+const whatsappNumber = getEnv("ACUIDAR_WHATSAPP_NUMBER") || "5511958189900";
+
 export const whatsAppDefaultLink =
-  `https://wa.me/${getEnv("ACUIDAR_WHATSAPP_NUMBER") || "5511958189900"}?text=Olá,%20gostaria%20de%20mais%20informações%20sobre%20os%20serviços%20da%20Acuidar.`;
+  `https://wa.me/${whatsappNumber}?text=Olá,%20gostaria%20de%20mais%20informações%20sobre%20os%20serviços%20da%20Acuidar.`;
 
 export const whatsAppAlternativeLink =
-  `https://wa.me/${getEnv("ACUIDAR_WHATSAPP_NUMBER") || "5511958189900"}?text=Olá!%20Gostaria%20de%20mais%20informações%20sobre%20os%20serviços%20da%20Acuidar.`;
+  `https://wa.me/${whatsappNumber}?text=Olá!%20Gostaria%20de%20mais%20informações%20sobre%20os%20serviços%20da%20Acuidar.`;
 
 export const workWithUsLink =
   getEnv("ACUIDAR_WORK_WITH_US_URL") || "https://whatsapp.com/channel/0029VbBeBr6Jf05Uk1rwlP3k";
